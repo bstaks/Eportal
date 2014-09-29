0d75bf9e1fd8bb3479ef9a48b1055bb00821bce6
Abhishek <basti_aks@yahoo.com>
2014-09-30 00:05:53 +0530

    add view model

---
+++ Eportal/EkartDataModel/ViewModel/MenuInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EkartDataModel.DataModel;
+namespace EkartDataModel.ViewModel
+{
+ public  class MenuInfo
+    {
+     public Menu _menu { get; set; }
+     public string Id { get; set; }
+     public string Name { get; set; }
+     public List<MenuInfo> _childMenu { get; set; }
+    }
+}
