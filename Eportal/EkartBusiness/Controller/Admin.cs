97c8919e864270c3afb3498ab3147e434f7beade
Abhishek <basti_aks@yahoo.com>
2014-09-29 22:01:19 +0530

    added business layer

---
+++ Eportal/EkartBusiness/Controller/Admin.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EkartDataModel.DataModel;
+namespace EkartBusiness.Controller
+{
+    public class Admin
+    {
+        public static IEnumerable<Menu> GetMenu()
+        {
+            try
+            {
+                using (MusicStoreEntities dbContext = new MusicStoreEntities())
+                {
+                    List<Menu> menu = dbContext.Menus.Where(m => m.MenuType == (int)EnumType.AdminMenuType.ParentMenu && m.ParentMenuId == 0).Select(m => m).ToList();
+                    List<Menu> GetMenu = new List<Menu>();
+                    foreach (Menu m in menu) {
+                        GetMenu.Add(m);
+                        GetMenu.AddRange(GetChildMenu(m.ParentMenuId.Value));
+                    }
+                    return GetMenu;
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                throw ex;
+            }
+
+        }
+
+        public static IEnumerable<Menu> GetChildMenu(int parentMenuId)
+        {
+            try
+            {
+                using (MusicStoreEntities dbContext = new MusicStoreEntities())
+                {
+                    List<Menu> menu = dbContext.Menus.Where(m =>m.ParentMenuId == parentMenuId).Select(m => m).ToList();
+                    List<Menu> GetMenu = new List<Menu>();
+                   
+                    foreach (Menu m in menu)
+                    {
+                        GetMenu.Add(m);
+                        GetMenu.AddRange(GetChildMenu(m.ParentMenuId.Value));
+                    }
+                    return GetMenu;
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Eportal/EkartBusiness/EnumType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EkartBusiness
+{
+    public class EnumType
+    {
+        public enum AdminMenuType
+        {
+            ParentMenu = 101,
+            ChildMenu = 102,
+            SubChild = 103
+        }
+    }
+}
