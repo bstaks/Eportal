ffcd439a9740d95488f4b128408f5c1c46751c42
Abhishek <basti_aks@yahoo.com>
2014-09-30 23:31:55 +0530

    new feature

--- Eportal/EkartBusiness/Controller/Admin.cs
+++ Eportal/EkartBusiness/Controller/Admin.cs
@@ -4,27 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EkartDataModel.DataModel;
+using EkartDataModel.ViewModel;
 namespace EkartBusiness.Controller
 {
     public class Admin
     {
-        public static IEnumerable<Menu> GetMenu()
+        public static IEnumerable<MenuInfo> GetMenu()
         {
             try
             {
-               
-                    using (MusicStoreEntities dbContext = new MusicStoreEntities())
+
+                using (MusicStoreEntities dbContext = new MusicStoreEntities())
+                {
+                    IEnumerable<Menu> menu = dbContext.Menus.Where(m => m.MenuType == (int)EnumType.AdminMenuType.ParentMenu && m.ParentMenuId == 0).Select(m => m).ToList();
+                    List<MenuInfo> GetMenu = new List<MenuInfo>();
+
+                    foreach (Menu m in menu.ToList())
                     {
-                        IEnumerable<Menu> menu = dbContext.Menus.Where(m => m.MenuType == (int)EnumType.AdminMenuType.ParentMenu && m.ParentMenuId == 0).Select(m => m).ToList();
-                        //List<Menu> GetMenu = new List<Menu>();
-                        //foreach (Menu m in menu) {
-                        //    GetMenu.Add(m);
-                        //    GetMenu.AddRange(GetChildMenu(m.ParentMenuId.Value));
-                        //}
-                        return menu;
+                        MenuInfo m1 = new MenuInfo();
+                        m1._menu = m;
+                        m1._childMenu = GetChildMenu(m.MenuId).ToList();
+                        GetMenu.Add(m1);
+
                     }
+                    return GetMenu;
                 }
-            
+            }
+
             catch (Exception ex)
             {
 
@@ -33,26 +39,29 @@ namespace EkartBusiness.Controller
 
         }
 
-        public static IEnumerable<Menu> GetChildMenu(int parentMenuId)
+        public static IEnumerable<MenuInfo> GetChildMenu(int MenuId)
         {
             try
             {
                 using (MusicStoreEntities dbContext = new MusicStoreEntities())
                 {
-                    List<Menu> menu = dbContext.Menus.Where(m =>m.ParentMenuId == parentMenuId).Select(m => m).ToList();
-                    List<Menu> GetMenu = new List<Menu>();
-                   
+
+                    List<Menu> menu = dbContext.Menus.Where(m => m.ParentMenuId == MenuId).Select(m => m).ToList();
+                    List<MenuInfo> GetMenu = new List<MenuInfo>();
+
                     foreach (Menu m in menu)
                     {
-                        GetMenu.Add(m);
-                        GetMenu.AddRange(GetChildMenu(m.ParentMenuId.Value));
+                        MenuInfo m1 = new MenuInfo();
+                        m1._menu = m;
+                        m1._childMenu = GetChildMenu(m.MenuId).ToList();
+                        GetMenu.Add(m1);
                     }
                     return GetMenu;
                 }
             }
             catch (Exception ex)
             {
-                
+
                 throw ex;
             }
         }
